402935f9e7e5450361094f5cab5cfa61446e63c3
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2025-04-19 14:25:00 +0200

    stage 2: idot scanner"

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,22 @@
 const std = @import("std");
+const process = std.process;
+const io = std.io;
+
+const Tokens = enum {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+};
 
 pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std.debug.print("Logs from your program will appear here!\n", .{});
 
     const args = try std.process.argsAlloc(std.heap.page_allocator);
-    defer std.process.argsFree(std.heap.page_allocator, args);
+    defer process.argsFree(std.heap.page_allocator, args);
 
     if (args.len < 3) {
         std.debug.print("Usage: ./your_program.sh tokenize <filename>\n", .{});
-        std.process.exit(1);
+        process.exit(1);
     }
 
     const command = args[1];
@@ -17,16 +24,34 @@ pub fn main() !void {
 
     if (!std.mem.eql(u8, command, "tokenize")) {
         std.debug.print("Unknown command: {s}\n", .{command});
-        std.process.exit(1);
+        process.exit(1);
     }
 
     const file_contents = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, filename, std.math.maxInt(usize));
     defer std.heap.page_allocator.free(file_contents);
 
+    const stdout = io.getStdOut();
     // Uncomment this block to pass the first stage
     if (file_contents.len > 0) {
-        @panic("Scanner not implemented");
+        const alloc = std.heap.page_allocator;
+        var tokens = std.ArrayList(Tokens).init(alloc);
+        defer tokens.deinit();
+
+        for (file_contents) |ch| {
+            // std.debug.print("eat `{c}`\n", .{ch});
+            if (ch == '\n') {
+                continue;
+            }
+            const token = switch (ch) {
+                '(' => Tokens.LEFT_PAREN,
+                ')' => Tokens.RIGHT_PAREN,
+                else => @panic("unimplemented"),
+            };
+            try stdout.writer().print("{s} {c} null\n", .{ @tagName(token), ch });
+            try tokens.append(token);
+        }
+        try stdout.writer().print("EOF\n", .{});
     } else {
-        try std.io.getStdOut().writer().print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
+        try io.getStdOut().writer().print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
     }
 }
